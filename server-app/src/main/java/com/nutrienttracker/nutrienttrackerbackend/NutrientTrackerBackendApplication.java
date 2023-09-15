9281122f33c9a332e3fa437ffda7101a976900ee
Michael <michael.henzi@hotmail.com>
2023-09-15 12:37:58 +0200

    set up spring boot project

---
+++ server-app/src/main/java/com/nutrienttracker/nutrienttrackerbackend/NutrientTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nutrienttracker.nutrienttrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrientTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrientTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ server-app/src/test/java/com/nutrienttracker/nutrienttrackerbackend/NutrientTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutrienttracker.nutrienttrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrientTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
